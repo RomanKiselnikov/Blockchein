d850d34ee2f302ff1f6c1c10c2cc55a2b4c7d320
RomanKiselnikov <nice.kiselnikov@inbox.ru>
2021-07-13 20:32:41 +0300

    test init

--- src/tests/test_blockchein.py
+++ src/tests/test_blockchein.py
@@ -3,6 +3,18 @@ import pytest
 from src.apps.blockchain import Blockchain
 
 
-def test_current_transactions():
-    bl = Blockchain()
-    assert bl.current_transactions == []
+@pytest.fixture(scope="module")
+def init_blockchain():
+    return Blockchain()
+
+
+def test_current_transactions(init_blockchain):
+    assert init_blockchain.current_transactions == []
+
+
+def test_valid_chain(init_blockchain):
+    assert type(init_blockchain.create_transaction('roma', 'nail', 200)) == int
+
+
+def test_current_transactions_len(init_blockchain):
+    assert len(init_blockchain.current_transactions) > 0
